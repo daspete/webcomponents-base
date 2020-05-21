a386f16dcfa3c2b2cda163c98f409d1fedee717d
daspete <daspetemail@gmail.com>
2020-05-21 12:29:48 +0200

    init

---
+++ components/Link.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="link">I am a link</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
---
+++ components/List.vue
@@ -0,0 +1,36 @@
+<template>
+    <ul class="list">
+        <li v-for="(item, itemId) in Items" :key="`item-${ itemId }`">
+            {{ item }}
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        items: { type: Array, default: () => { return [] } } 
+    },
+
+    computed: {
+        Items(){
+            // Hacky solution for array and object props
+            return this.$parent.$options.customElement.items || this.items
+        }
+    },
+
+    mounted(){
+
+    },
+
+    methods: {
+        
+    }
+}
+</script>
+
+<style lang="scss">
+.btn {
+    
+}
+</style>
